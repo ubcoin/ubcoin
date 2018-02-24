a2683155adf9f911a57373efbd597e23c4487dc4
natyur <natalia.yurkovets@gmail.com>
2018-02-25 00:10:01 +0300

    chore(test): add preico tests

---
+++ test/preico.js
@@ -0,0 +1,41 @@
+import ether from './helpers/ether';
+import tokens from './helpers/tokens';
+import unixTime from './helpers/unixTime';
+import {duration} from './helpers/increaseTime';
+
+import capped from './preico/capped';
+import common from './preico/common';
+import additional from './preico/additional';
+
+const token = artifacts.require('UBCoinToken.sol');
+const crowdsale = artifacts.require('PreICO.sol');
+
+contract('ITO - common test', function (accounts) {
+  before(config);
+  common(token, crowdsale, accounts);
+});
+
+contract('ITO - capped crowdsale test', function (accounts) {
+  before(config);
+  capped(token, crowdsale, accounts);
+});
+
+contract('ITO - additional features test', function (accounts) {
+  before(config);
+  additional(token, crowdsale, accounts);
+});
+
+function config() {
+  // variables list based on info from README
+  this.start = unixTime('01 Mar 2018 00:00:00 GMT');
+  this.period = 15;
+  this.price = tokens(33334);
+  this.hardcap = ether(8500);
+  this.minInvestedLimit = ether(0.1);
+  this.wallet = '0xa86780383E35De330918D8e4195D671140A60A74';
+
+  // variables for additional testing convinience
+  this.end = this.start + duration.days(this.period);
+  this.beforeStart = this.start - duration.seconds(10);
+  this.afterEnd = this.end + duration.seconds(1);
+}
---
+++ test/preico/common.js
@@ -0,0 +1,77 @@
+import ether from '../helpers/ether';
+import tokens from '../helpers/tokens';
+import {advanceBlock} from '../helpers/advanceToBlock';
+import {increaseTimeTo, duration} from '../helpers/increaseTime';
+import latestTime from '../helpers/latestTime';
+import EVMRevert from '../helpers/EVMRevert';
+import unixTime from '../helpers/unixTime';
+
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(web3.BigNumber))
+  .should();
+
+export default function (Token, Crowdsale, wallets) {
+  let token;
+  let crowdsale;
+
+  before(async function () {
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock();
+  });
+
+  beforeEach(async function () {
+    token = await Token.new();
+    crowdsale = await Crowdsale.new();
+    await token.setSaleAgent(crowdsale.address);
+    await crowdsale.setToken(token.address);
+    await crowdsale.setStart(this.start);
+    await crowdsale.setPeriod(this.period);
+    await crowdsale.setPrice(this.price);
+    await crowdsale.setHardcap(this.hardcap);
+    await crowdsale.setMinInvestedLimit(this.minInvestedLimit);
+    await crowdsale.setWallet(this.wallet);
+  });
+
+  it('crowdsale should be a saleAgent for token', async function () {
+    const owner = await token.saleAgent();
+    owner.should.equal(crowdsale.address);
+  });
+
+
+  it('end should be equal to start + duration', async function () {
+    const start = await crowdsale.start();
+    const end = await crowdsale.endSaleDate();
+    end.should.bignumber.equal(start.plus(duration.days(this.period)));
+  });
+
+  it('should reject payments before start', async function () {
+    await crowdsale.setStart(this.start + duration.seconds(1));
+    await crowdsale.sendTransaction({value: ether(1), from: wallets[3]}).should.be.rejectedWith(EVMRevert);
+  });
+
+  it('should accept payments after start', async function () {
+    await increaseTimeTo(this.start + duration.seconds(1));
+    await crowdsale.sendTransaction({value: ether(1), from: wallets[3]}).should.be.fulfilled;
+  });
+
+  it('should reject payments after finish', async function () {
+    await increaseTimeTo(this.start + duration.seconds(10));
+    await crowdsale.sendTransaction({value: ether(1), from: wallets[3]}).should.be.fulfilled;
+    const owner = await crowdsale.owner();
+    await crowdsale.finish({from: owner}).should.be.fulfilled;
+    await crowdsale.sendTransaction({value: ether(1), from: wallets[3]}).should.be.rejectedWith(EVMRevert);
+  });
+
+  it('should assign tokens to sender', async function () {
+    await crowdsale.sendTransaction({value: ether(1), from: wallets[3]});
+    const balance = await token.balanceOf(wallets[3]);
+    balance.should.be.bignumber.equal(this.price);
+  });
+
+  it('should reject payments after end', async function () {
+    await increaseTimeTo(this.afterEnd);
+    await crowdsale.sendTransaction({value: ether(1), from: wallets[3]}).should.be.rejectedWith(EVMRevert);
+  });
+
+}
