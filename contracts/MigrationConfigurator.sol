81e82a901f842d48874f013da321f9fd76c66779
Alexander Strakh <strakh@ispras.ru>
2018-04-24 14:32:19 +0300

    Prepare migration contract

---
+++ contracts/MigrationConfigurator.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.4.18;
+
+import './ownership/Ownable.sol';
+import './MintableToken.sol';
+import './UBCoinToken.sol';
+import './PreICO.sol';
+import './ICO.sol';
+import './FreezeTokensWallet.sol';
+
+/**
+ * How to migrate:
+ * 1. deploy
+ * 2. call deploy
+ * 3. token.setSaleAgent(new ito)
+ */
+contract MigrationConfigurator is Ownable {
+
+  MintableToken public token = MintableToken(0x2D3E7D4870a51b918919E7B851FE19983E4c38d5);
+
+  ICO public ico;
+
+  FreezeTokensWallet public teamTokensWallet;
+
+  function setToken(address newAddress) public onlyOwner {
+    token = MintableToken(newAddress);
+  }
+
+  function deploy() public onlyOwner {
+    ico = new ICO();
+
+    ico.addMilestone(20, 40);
+    ico.addMilestone(20, 25);
+    ico.addMilestone(20, 20);
+    ico.addMilestone(20, 15);
+    ico.addMilestone(20, 8);
+    ico.addMilestone(4, 0);
+    ico.addValueBonus(20000000000000000000,50);
+    ico.addValueBonus(50000000000000000000,65);
+    ico.addValueBonus(300000000000000000000,80);
+    ico.setMinInvestedLimit(100000000000000000);
+    ico.setToken(token);
+    ico.setPrice(14286000000000000000000);
+    ico.setWallet(0x5FB78D8B8f1161731BC80eF93CBcfccc5783356F);
+    ico.setBountyTokensWallet(0xdAA156b6eA6b9737eA20c68Db4040B1182E487B6);
+    ico.setReservedTokensWallet(0xE1D1898660469797B22D348Ff67d54643d848295);
+    ico.setStart(1522627200); // 02 Apr 2018 00:00:00 GMT
+    ico.setHardcap(96000000000000000000000);
+    ico.setTeamTokensPercent(12);
+    ico.setBountyTokensPercent(4);
+    ico.setReservedTokensPercent(34);
+    ico.addValueBonus()
+
+    teamTokensWallet = new FreezeTokensWallet();
+    teamTokensWallet.setStartLockPeriod(180);
+    teamTokensWallet.setPeriod(360);
+    teamTokensWallet.setDuration(90);
+    teamTokensWallet.setToken(token);
+    teamTokensWallet.transferOwnership(ico);
+
+    ico.setTeamTokensWallet(teamTokensWallet);
+
+    address manager = 0xF1f94bAD54C8827C3B53754ad7dAa0FF5DCD527d;
+
+    ico.transferOwnership(manager);
+  }
+
+}
+
